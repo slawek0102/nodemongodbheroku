4a76dc15e38da67493f8d5f8a27d9fb6e0eca3b8
Slawek0102 <grzebyk.slawomir@gmail.com>
2017-09-07 20:51:40 +0200

    Index.js changed

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+var port = process.env.PORT || 8080
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log('Example app listening on port 3000!')
 })
