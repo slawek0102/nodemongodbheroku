75c5ab03d19b6d984be5a54b846f9686f491e4d6
Slawek0102 <grzebyk.slawomir@gmail.com>
2017-09-07 20:53:56 +0200

    Index.js changed

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
+app.get('/ala', function (req, res) {
+    res.send('ALA dziala!')
+})
+
 app.listen(port, function () {
     console.log('Example app listening on port 3000!')
 })
