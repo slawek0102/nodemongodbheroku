3be10d3966732545049275b8a520c970c46eb1a9
Slawek0102 <grzebyk.slawomir@gmail.com>
2017-09-07 22:48:56 +0200

    aa

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express')
-const app = express()
+const app = express();
 
-var port = process.env.PORT || 8080
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
-})
+});
 
 app.get('/ala', function (req, res) {
     res.send('ALA dziala!')
-})
+});
 
 app.listen(port, function () {
     console.log('Example app listening on port 3000!')
-})
+});
